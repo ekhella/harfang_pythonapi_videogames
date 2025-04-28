5abd1049f7e1df5525571387d34ee37dd9a11220
ekhella <eric.khella@imt-atlantique.net>
2025-04-28 20:13:35 +0200

    Route POST pour créer un jeu

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from typing import List
+from models import VideoGame
 
 app = FastAPI()
+
+# Fausse base de données en mémoire
+games_db: List[VideoGame] = []
 
 @app.get("/")
 def read_root():
     return {"message": "Bienvenue sur l'API de la base de données de jeux vidéo !"}
+
+@app.post("/games/", response_model=VideoGame)
+def create_game(game: VideoGame):
+    # Vérifie si le jeu existe déjà exactement (on fera fuzzy plus tard)
+    for existing_game in games_db:
+        if existing_game.name.lower() == game.name.lower():
+            raise HTTPException(status_code=400, detail="Un jeu avec ce nom existe déjà.")
+
+    games_db.append(game)
+    return game
